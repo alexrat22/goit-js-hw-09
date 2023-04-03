2aaaff3d4e988cc96966f54503e790cb0bdcb9fc
alexrat22 <117772110+alexrat22@users.noreply.github.com>
2023-04-03 17:12:04 +0000

    Deploying to gh-pages from @ alexrat22/goit-js-hw-09@3c30fcfc16501320cf93ad0853f71269e9816f2a 🚀

---
+++ 01-color-switcher.7f95ba95.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.7f95ba95.js.map
