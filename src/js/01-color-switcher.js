3c30fcfc16501320cf93ad0853f71269e9816f2a
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-04-03 20:11:41 +0300

    done Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const backgroundArea = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  timerId = setInterval(() => {
+    backgroundArea.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', '');
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  startButton.removeAttribute('disabled', '');
+}
