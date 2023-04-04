baad8f26e3eced728005d768b2e66b9a1208141d
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-04-04 08:44:20 +0300

    update Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 const backgroundArea = document.querySelector('body');
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
