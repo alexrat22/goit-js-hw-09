d5f49acf0e0eff470d29c6326e4937ce099837a8
alexrat22 <117772110+alexrat22@users.noreply.github.com>
2023-04-04 05:44:50 +0000

    Deploying to gh-pages from @ alexrat22/goit-js-hw-09@baad8f26e3eced728005d768b2e66b9a1208141d 🚀

---
+++ 01-color-switcher.be5a2c0b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(function(){r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.be5a2c0b.js.map
--- 01-color-switcher.eb5baf6d.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled","")}));
-//# sourceMappingURL=01-color-switcher.eb5baf6d.js.map
