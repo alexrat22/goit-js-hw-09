c957fae73337be505d2a5abfbd07c498852116c4
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-04-07 12:50:58 +0300

    Task 3 changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,52 +1,38 @@
-import Notiflix, { Loading } from 'notiflix';
-
-const STORAGE_KEY = 'Form-Key';
-const formData = {};
+import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve(
-        Notiflix.Notify.success(`Fullfiled promise ${position} in ${delay}ms`)
-      );
-    } else {
-      reject(
-        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`)
-      );
-    }
-    return promise;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+  return promise;
 }
 
-const refs = {
-  form: document.querySelector('.form'),
-  createButton: document.querySelector('button'),
-  firstDelay: document.querySelector('[name = delay]'),
-  delayStep: document.querySelector('[name = step]'),
-  amount: document.querySelector('[name = amount]'),
-};
-refs.form.addEventListener('input', onFormInput);
-refs.createButton.addEventListener('click', onSubmitButtonClick);
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitClick);
 
-function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+function onSubmitClick(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+
+  for (let i = 1; i <= Number(evt.currentTarget.amount.value); i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    delay += Number(evt.currentTarget.step.value);
+  }
 }
-
-function onSubmitButtonClick() {
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  const parsedData = JSON.parse(savedData);
-  let position = 0;
-  const intervalId = setInterval(() => {
-    position += 1;
-    createPromise(position, 10000);
-    if (position === parsedData.amount) {
-      clearInterval(intervalId);
-    }
-  }, 1000);
-}
-
-//   console.log(refs.firstDelay.value);
-//   console.log(refs.delayStep.value);
-//   console.log(refs.amount.value);
